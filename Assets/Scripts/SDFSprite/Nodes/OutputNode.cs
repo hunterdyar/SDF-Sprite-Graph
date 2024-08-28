852e492e85e2ec6440db6799371b7194014282b2
Hunter Dyar <bloop@hdyar.com>
2024-08-28 16:20:59 -0400

    Initial commit

---
+++ Assets/Scripts/SDFSprite/Nodes/OutputNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+using UnityEditor.Experimental.GraphView;
+
+[System.Serializable]
+public class OutputNode : Node
+{
+	public Color BackgroundColor;
+	public Color ForegroundColor;
+	public int Width;
+	public int Height;
+}
---
+++ Assets/Scripts/SDFSprite/SDFSprite.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SDFSprite", menuName = "SDFSprite/SDFSprite", order = 1)]
+public class SDFSprite : ScriptableObject
+{
+   //todo: child asset. How did we do it in daily golf gen?
+   private Texture2D _spriteTexture;
+   
+   
+}
